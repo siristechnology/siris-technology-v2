a048d9c86435daa9b517dbce422413ad6d90be08
Suraj Shrestha <syuraj@gmail.com>
2019-07-20 10:40:54 -0400

    Using actual fields instead of gatsby fragment as it causes about page not to be built

--- src/components/landing/Work/index.jsx
+++ src/components/landing/Work/index.jsx
@@ -6,7 +6,11 @@ import SideProjects from './SideProjects'
 export const imageFields = graphql`
 	fragment imageFields on ImageSharp {
 		fluid(maxWidth: 630) {
-			...GatsbyImageSharpFluid_tracedSVG
+			tracedSVG
+			aspectRatio
+			src
+			srcSet
+			sizes
 		}
 	}
 `
